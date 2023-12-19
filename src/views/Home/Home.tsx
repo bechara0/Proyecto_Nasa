6b3c64e74c67440b49a6388ecc48f68e0b886248
bechara0 <jjbechara@gmail.come>
2023-12-19 17:51:28 -0300

    funciona 5 dias

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
+import { format, sub } from "date-fns";
 import Header from "../../components/Header/Header";
 import TodaysImage from "../../components/TodaysImage";
 import fetchApi from "../../utils/fetch";
 import { PostImage } from "../../types";
+import LasFiveDaysImages from "../../components/LastFiveDaysImages/LasFiveDaysImages";
 
 const Home = () => {
   const [todaysImage, setTodaysImage] = useState<PostImage>();
-
+  const [lastFiveDaysImages, setLastFiveDaysImages] = useState<PostImage[]>([]);
   useEffect(() => {
     const loadTodayImage = async () => {
       try {
@@ -18,15 +20,30 @@ const Home = () => {
         setTodaysImage({});
       }
     };
-    loadTodayImage().catch(null);
-  }, []);
 
-  console.log("todaysImage: ", todaysImage);
+    const loadLast5DaysImages = async () => {
+      try {
+        const date = new Date();
+        const todaysDate = format(date, "yyyy-MM-dd");
+        const fiveDaysAgoDate = format(sub(date, { days: 5 }), "yyyy-MM-dd");
+        const lastFiveDaysImagesResponse = await fetchApi(
+          `&start_date=${fiveDaysAgoDate}&end_date=${todaysDate}`
+        );
+        setLastFiveDaysImages(lastFiveDaysImagesResponse);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadTodayImage().catch(null);
+    loadLast5DaysImages().catch(null);
+  }, []);
 
   return (
     <View style={styles.container}>
       <Header />
       <TodaysImage {...todaysImage} />
+      <LasFiveDaysImages postImages={lastFiveDaysImages} />
     </View>
   );
 };
